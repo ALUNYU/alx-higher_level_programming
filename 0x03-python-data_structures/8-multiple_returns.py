09088043bec9a9920222f9f232be44016204a342
ALUNYU <desmondpatkins24@gmail.com>
2023-03-13 07:29:56 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-
-
 def multiple_returns(sentence):
-    if sentence is not None:
-        sent_len = len(sentence)
-        if sent_len == 0:
-            return (sent_len, None)
-        return (sent_len, sentence[0])
+    length = len(sentence)
+    char = sentence[0] if length > 0 else None
 
-
-if __name__ == '__main__':
-    sentence = "At Holberton school, I learnt C!"
-    length, first = multiple_returns(sentence)
-    print("Length: {:d} - First character: {}".format(length, first))
-
+    return (length, char)
